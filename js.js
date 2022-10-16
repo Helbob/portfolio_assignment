47b8b4010e6b570424e7092d6638f5118acc521f
Helbob <millehelbo@outlook.dk>
2022-10-16 18:56:47 +0200

    animtaion

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
 "use strict";
+
+import {
+  animate,
+  scroll,
+  stagger,
+  inView,
+} from "https://cdn.skypack.dev/motion";
+scroll(animate(".progress", { scaleX: [0, 1] }));
+animate("body", { opacity: [0, 1] }, { duration: 1.5 });
